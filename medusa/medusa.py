e565a134fa62be956777d452f307091c47a92e3a
Marcelo Machado <mjoelmachadojr@gmail.com>
2024-09-21 22:57:37 -0300

    moved project files to .github && started medusa.py

--- medusa/medusa.py
+++ medusa/medusa.py
@@ -1,20 +1,20 @@
-#!/usr/bin/python
-# 
+#!/bin/usr/python
+#
 # Filename: medusa.py
 # Description: Medusa is a multipurpose hacking tool.
 # Author: marques <uniqueduckinbox at duck.com>
 # URL: https://github.com/mmaachado/medusa
 # Keywords: Medusa Offensive Security PenTest Red Team
-# Compatibility: python-version >= 3.10.5
-# 
+# Compatibility: python-version >= 3.12.5
+#
 # ----------------------------
-# 
+#
 # Commentary:
-# 
+#
 # this is the start of Medusa
-# 
+#
 # ----------------------------
-# 
+#
 # This program is a free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or (at
@@ -26,8 +26,18 @@
 # You should have received a copy of the GNU General Public License
 # along with your GNU/Linux distribution.  If not, see <https://www.gnu.org/licenses/>.
 
-def medusa():
-    ...
 
-if __name__ == '__main__':
-    medusa()
+from colorama import Fore
+
+banner: str = r"""
+ __    __    ______    _____     __  __    ______    ______    
+/\ "-./  \  /\  ___\  /\  __-.  /\ \/\ \  /\  ___\  /\  __ \   
+\ \ \-./\ \ \ \  __\  \ \ \/\ \ \ \ \_\ \ \ \___  \ \ \  __ \  
+ \ \_\ \ \_\ \ \_____\ \ \____-  \ \_____\ \/\_____\ \ \_\ \_\ 
+  \/_/  \/_/  \/_____/  \/____/   \/_____/  \/_____/  \/_/\/_/ 
+                                                                   
+v 0.1
+
+"""
+
+print(Fore.GREEN + banner)
